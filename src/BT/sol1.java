4637785dcc01a599126125bac61b7ffd5bc59263
Ankur <ank.chauhan840@gmail.com>
2022-09-12 02:39:32 +0530

    first commit

---
+++ src/BT/sol1.java
@@ -0,0 +1,7 @@
+package BT;
+
+public class sol1 {
+    public static void main(String[] args) {
+        System.out.println("Sol 1");
+    }
+}
---
+++ src/Sliding_Window/Que1.java
@@ -0,0 +1,7 @@
+package Sliding_Window;
+
+public class Que1 {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
